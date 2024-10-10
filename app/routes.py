c1cc9632517e7b7005a80558eace4bba3209eb40
PedroMoraes39 <phcmoraes39@gmail.com>
2024-10-10 12:36:05 -0300

    Structuring Web Forms

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,27 @@
 from flask import render_template
+from flask import flash
+from flask import redirect
+from flask import url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
+
 @app.route('/index')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    
+    form = LoginForm()
+    
+    if form.validate_on_submit():
+        
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        
+        return redirect(url_for('index'))
+    
+    return render_template('login.html', title='Sign In', form=form)
 
 def index():
     
