ce6a57f44c590c8ea2589429af0d17e1654faf2a
PedroMoraes39 <phcmoraes39@gmail.com>
2025-02-07 23:47:12 -0300

    Adding and running pre-commit

--- app/forms.py
+++ app/forms.py
@@ -1,50 +1,46 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms import PasswordField
-from wtforms import BooleanField
-from wtforms import SubmitField
-from wtforms import TextAreaField
-from wtforms.validators import ValidationError
-from wtforms.validators import Email
-from wtforms.validators import EqualTo
-from wtforms.validators import DataRequired
-from wtforms.validators import Length
 import sqlalchemy
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, PasswordField, StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+
 from app import db
 from app.models import User
 
+
 class LoginForm(FlaskForm):
-    
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember_me = BooleanField("Remember Me")
+    submit = SubmitField("Sign In")
+
 
 class RegistrationForm(FlaskForm):
-    
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
     password2 = PasswordField(
-        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Register')
-    
+        "Repeat Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    submit = SubmitField("Register")
+
     def validate_username(self, username):
-        
-        user = db.session.scalar(sqlalchemy.select(User).where(
-            User.username == username.data))
-    
-    def validate_email(self, email):
-        
-        user = db.session.scalar(sqlalchemy.select(User).where(
-            User.email == email.data))
-        
+        user = db.session.scalar(
+            sqlalchemy.select(User).where(User.username == username.data)
+        )
+
         if user is not None:
-            
-            raise ValidationError('Please use a different email address.')
-        
+            raise ValidationError("Please use a different username.")
+
+    def validate_email(self, email):
+        user = db.session.scalar(
+            sqlalchemy.select(User).where(User.email == email.data)
+        )
+
+        if user is not None:
+            raise ValidationError("Please use a different email address.")
+
+
 class EditProfileForm(FlaskForm):
-    
-    username = StringField('Username', validators=[DataRequired()])
-    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
-    submit = SubmitField('Submit')
+    username = StringField("Username", validators=[DataRequired()])
+    about_me = TextAreaField("About me", validators=[Length(min=0, max=140)])
+    submit = SubmitField("Submit")
--- app/models.py
+++ app/models.py
@@ -1,74 +1,80 @@
-from datetime import datetime
-from datetime import timezone
+from datetime import datetime, timezone
+from hashlib import md5
 from typing import Optional
+
 import sqlalchemy
 import sqlalchemy.orm
-from app import db
-from werkzeug.security import generate_password_hash
-from werkzeug.security import check_password_hash
 from flask_login import UserMixin
-from app import login
-from hashlib import md5
+from werkzeug.security import check_password_hash, generate_password_hash
 
-class User(UserMixin ,db.Model):
-    
+from app import db, login
+
+
+class User(UserMixin, db.Model):
     id: sqlalchemy.orm.Mapped[int] = sqlalchemy.orm.mapped_column(primary_key=True)
-    
-    username: sqlalchemy.orm.Mapped[str] = sqlalchemy.orm.mapped_column(sqlalchemy.String(64), 
-                                                index=True,
-                                                unique=True)
-    
-    email: sqlalchemy.orm.Mapped[str] = sqlalchemy.orm.mapped_column(sqlalchemy.String(120), 
-                                             index=True,
-                                             unique=True)
-    
-    password_hash: sqlalchemy.orm.Mapped[Optional[str]] = sqlalchemy.orm.mapped_column(sqlalchemy.String(256))
-    
+
+    username: sqlalchemy.orm.Mapped[str] = sqlalchemy.orm.mapped_column(
+        sqlalchemy.String(64), index=True, unique=True
+    )
+
+    email: sqlalchemy.orm.Mapped[str] = sqlalchemy.orm.mapped_column(
+        sqlalchemy.String(120), index=True, unique=True
+    )
+
+    password_hash: sqlalchemy.orm.Mapped[Optional[str]] = sqlalchemy.orm.mapped_column(
+        sqlalchemy.String(256)
+    )
+
     last_seen: sqlalchemy.orm.Mapped[Optional[datetime]] = sqlalchemy.orm.mapped_column(
-        default=lambda: datetime.now(timezone.utc))
-    
-    posts: sqlalchemy.orm.WriteOnlyMapped['Post'] = sqlalchemy.orm.relationship(
-        back_populates='author')
-    
-    about_me: sqlalchemy.orm.Mapped[Optional[str]] = sqlalchemy.orm.mapped_column(sqlalchemy.String(140))
+        default=lambda: datetime.now(timezone.utc)
+    )
+
+    posts: sqlalchemy.orm.WriteOnlyMapped["Post"] = sqlalchemy.orm.relationship(
+        back_populates="author"
+    )
+
+    about_me: sqlalchemy.orm.Mapped[Optional[str]] = sqlalchemy.orm.mapped_column(
+        sqlalchemy.String(140)
+    )
 
     def __repr__(self):
-        
-        return '<User {}>'.format(self.username)
-    
+        return f"<User {self.username}>"
+
     def set_password(self, password):
-        
         self.password_hash = generate_password_hash(password)
-    
+
     def check_password(self, password):
-    
         return check_password_hash(self.password_hash, password)
-    
+
     def avatar(self, size):
-        
-        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-        
-        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
+        digest = md5(self.email.lower().encode("utf-8")).hexdigest()
+
+        return f"https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}"
+
 
 class Post(db.Model):
-    
     id: sqlalchemy.orm.Mapped[int] = sqlalchemy.orm.mapped_column(primary_key=True)
-    
-    body: sqlalchemy.orm.Mapped[str] = sqlalchemy.orm.mapped_column(sqlalchemy.String(140))
-    
-    timestamp: sqlalchemy.orm.Mapped[datetime] = sqlalchemy.orm.mapped_column(index=True,
-                                                      default=lambda: datetime.now(timezone.utc))
-    
-    user_id: sqlalchemy.orm.Mapped[int] = sqlalchemy.orm.mapped_column(sqlalchemy.ForeignKey(User.id),
-                                               index=True)
-    
-    author: sqlalchemy.orm.Mapped[User] = sqlalchemy.orm.relationship(back_populates='posts')
-    
+
+    body: sqlalchemy.orm.Mapped[str] = sqlalchemy.orm.mapped_column(
+        sqlalchemy.String(140)
+    )
+
+    timestamp: sqlalchemy.orm.Mapped[datetime] = sqlalchemy.orm.mapped_column(
+        index=True, default=lambda: datetime.now(timezone.utc)
+    )
+
+    user_id: sqlalchemy.orm.Mapped[int] = sqlalchemy.orm.mapped_column(
+        sqlalchemy.ForeignKey(User.id), index=True
+    )
+
+    author: sqlalchemy.orm.Mapped[User] = sqlalchemy.orm.relationship(
+        back_populates="posts"
+    )
+
     def __repr__(self):
-        
-        return '<Post {}>'.format(self.body)
+        return f"<Post {self.body}>"
+
 
 @login.user_loader
 def load_user(id):
-    
     return db.session.get(User, int(id))
